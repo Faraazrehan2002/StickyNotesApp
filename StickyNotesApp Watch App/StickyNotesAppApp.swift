6deb85698fec3101c929c6299f7542e31307e6c6
Faraazrehan2002 <faraazrehan2002@gmail.com>
2024-11-01 21:40:30 -0500

    Initial Commit

---
+++ StickyNotesApp Watch App/StickyNotesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StickyNotesAppApp.swift
+//  StickyNotesApp Watch App
+//
+//  Created by Faraaz Rehan Junaidi Mohammed on 11/1/24.
+//
+
+import SwiftUI
+
+@main
+struct StickyNotesApp_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ StickyNotesApp Watch AppTests/StickyNotesApp_Watch_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  StickyNotesApp_Watch_AppTests.swift
+//  StickyNotesApp Watch AppTests
+//
+//  Created by Faraaz Rehan Junaidi Mohammed on 11/1/24.
+//
+
+import Testing
+@testable import StickyNotesApp_Watch_App
+
+struct StickyNotesApp_Watch_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
