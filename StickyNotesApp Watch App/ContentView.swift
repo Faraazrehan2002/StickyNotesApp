d07a4a18226f09a6df1b8723865ef8ad5d40b910
Faraazrehan2002 <faraazrehan2002@gmail.com>
2024-11-01 22:38:15 -0500

    Content VIew Updated

--- StickyNotesApp Watch App/ContentView.swift
+++ StickyNotesApp Watch App/ContentView.swift
@@ -8,14 +8,26 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var stickyNotes: [StickyNote] = [StickyNote]()
+    @State private var text: String = ""
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+            TextField("Enter new note", text: $text)
+            
+            Button{
+                guard !text.isEmpty else { return }
+                
+                let newNote = StickyNote(id: UUID(), text: text)
+                stickyNotes.append(newNote)
+                text = ""
+            }label: {
+                Image(systemName: "plus.circle")
+                    .font(.system(size: 42, weight: .semibold))
+            }
+            .fixedSize()
+            .foregroundColor(.accent)
+        } //VSTACK
     }
 }
 
---
+++ StickyNotesApp Watch App/StickyNote.swift
@@ -0,0 +1,13 @@
+//
+//  StickyNote.swift
+//  StickyNotesApp Watch App
+//
+//  Created by Faraaz Rehan Junaidi Mohammed on 11/1/24.
+//
+
+import Foundation
+
+struct StickyNote {
+    let id: UUID
+    let text: String
+}
